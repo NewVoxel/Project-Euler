4ac498e46d5ae12c73c16c5836b202c80460ce6f
Alex Woodcroft <newvoxel@gmail.com>
2016-03-07 23:39:17 +1000

    Problem 009 Modified

--- ProjectEulerSolutions/Problem009.cs
+++ ProjectEulerSolutions/Problem009.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ProjectEulerSolutions
 {
     class Problem009
     {
+        public void run()
+        {
+            //Create a stopwatch to calculate how long program takes to execute
+            var watch = Stopwatch.StartNew();
+
+            //Display time taken to execute program
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.Out.Write("Program completed in : " + elapsedMs + "ms");
+        }
     }
 }
